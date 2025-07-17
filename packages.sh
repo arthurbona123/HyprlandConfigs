8b92441dc29892dc848061f6a15b873f41f5cb23
Arthur Bonachela Fabiano <160297560+arthurbona123@users.noreply.github.com>
2025-07-16 22:18:53 -0300

    Create packages.sh

---
+++ packages.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pacman -S  flatpak firefox kitty udiskie nemo spectacle git fuse gcc cmake make qt5ct qt6ct kvantum iwgtk hyprpolkitagent dhcpcd drawing inkscape skanlite gimp vlc arduino-ide virtualbox libreoffice-fresh codeblocks hwinfo vi vim evince nvidia-utils cups cups-browsed pavucontrol blueman brightnessctl copyq system-printer-config mysql gamemode qjackctl pipewire-jack zsh gnu-free-fonts openrgb waybar hypridle hyprlock hyprpaper nwg-bar nwg-look nwg-icon-picker rofi
+
+yay -S google-chrome pycharm-community-edition code escpr sonobus reaper sddm-silent-theme fontbase flatseal
+
+flatpak install steam mangohud obsidian flatseal
+
+wget -4 https://github.com/DavidoTek/ProtonUp-Qt/releases/download/v2.13.0/ProtonUp-Qt-2.13.0-x86_64.AppImage
+wget -4 https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.17.2/Heroic-2.17.2-linux-x86_64.AppImage
+wget -4 https://github.com/PCSX2/pcsx2/releases/download/v2.4.0/pcsx2-v2.4.0-linux-appimage-x64-Qt.AppImage
+wget -4 https://github.com/RPCS3/rpcs3-binaries-linux/releases/download/build-9c93ec0bc31bbc94ca4dce2a76ceea80da6f6554/rpcs3-v0.0.37-18022-9c93ec0b_linux64.AppImage
+
+mv ProtonUp-Qt-2.13.0-x86_64.AppImage /home/$USER/AppImages
+mv Heroic-2.17.2-linux-x86_64.AppImage /home/$USER/AppImages
+mv pcsx2-v2.4.0-linux-appimage-x64-Qt.AppImage /home/$USER/AppImages
+mv rpcs3-v0.0.37-18022-9c93ec0b_linux64.AppImage /home/$USER/AppImages
