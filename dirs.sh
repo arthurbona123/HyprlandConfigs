047b63556a2039fb226873c89ec4bc0c7ed6fc7a
Arthur Bonachela Fabiano <160297560+arthurbona123@users.noreply.github.com>
2025-07-16 22:04:45 -0300

    Create dirs.sh

---
+++ dirs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir /home/$USER/Downloads
+mkdir /home/$USER/Documents
+mkdir /home/$USER/Pictures
+mkdir /home/$USER/Videos
+mkdir /home/$USER/Music
+mkdir /home/$USER/AppImages
+
+cp /etc/xdg/waybar /home/$USER/.config/ -r
+cp /etc/xdg/nwg-bar /home/$USER/.config/ -r 
+
+mkdir /home/$USER/.config/rofi -pv
+rofi -dump-config > /home/$USER/.config/rofi/config.rasi
+echo "\n\n\n\n" >> /home/$USER/.config/rofi/config.rasi
+rofi -dump-theme >> /home/$USER/.config/rofi/config.rasi
+
+touch /home/$USER/.config/hypr/hypridle.conf
+touch /home/$USER/.config/hypr/hyprlock.conf
+touch /home/$USER/.config/hypr/hyprpaper.conf
+touch /home/$USER/.config/electron-flags.conf
+echo "--enable-features=UseOzonePlatform\n--ozone-platform=wayland" >> /home/$USER/.config/electron-flags.conf
+
+mkdir /home/$USER/Pictures/backgrounds -pv
+
+mkdir /usr/share/zsh-themes -pv
+mkdir /usr/share/zsh-plugins -pv
