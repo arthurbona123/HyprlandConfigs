5f3caf48e041f38e664965f6aa4b146cc573aa78
Arthur Bonachela Fabiano <160297560+arthurbona123@users.noreply.github.com>
2025-08-19 21:54:57 -0300

    Update services.sh

--- services.sh
+++ services.sh
@@ -4,8 +4,10 @@ systemctl enable iwd.service
 systemctl disable NetworkManager
 systemctl enable bluetooth.service
 systemctl enable cups
+systemctl enable swayosd-libinput-backend.service
 
 systemctl start iwd.service
 systemctl start bluetooth.service
 systemctl stop NetworkManager
 systemctl start cups
+systemctl start swayosd-libinput-backend.service
