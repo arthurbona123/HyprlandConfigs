d94baa54719ebdaf9dfdd5cdd7b4084248e9ffcc
Arthur Bonachela Fabiano <160297560+arthurbona123@users.noreply.github.com>
2025-09-03 12:38:40 -0300

    Update services.sh

--- services.sh
+++ services.sh
@@ -5,9 +5,11 @@ systemctl disable NetworkManager
 systemctl enable bluetooth.service
 systemctl enable cups
 systemctl enable swayosd-libinput-backend.service
+systemctl enable dhcpcd
 
 systemctl start iwd.service
 systemctl start bluetooth.service
 systemctl stop NetworkManager
 systemctl start cups
 systemctl start swayosd-libinput-backend.service
+systemctl start dhcpcd
